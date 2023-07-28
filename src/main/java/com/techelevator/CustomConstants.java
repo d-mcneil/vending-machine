9296a8b20b2274c3f4a951caee112c9112cda6b4
Dean McNeil <deanolmcneil@gmail.com>
2023-07-28 17:40:56 -0400

    Pull constant values out of VendingMachine and into CustomConstants

--- src/main/java/com/techelevator/CustomConstants.java
+++ src/main/java/com/techelevator/CustomConstants.java
@@ -1,7 +1,18 @@
 package com.techelevator;
 
 public class CustomConstants {
-    public static String starChar = "☆";
-    public static String welcomeMessage = "Welcome to Dean and Michel's Vending Machine";
-    public static String exitMessage = "Thanks for Using Dean and Michel's Vending Machine";
+    public static final String starChar = "☆";
+    public static final String welcomeMessage = "Welcome to Dean and Michel's Vending Machine";
+    public static final String exitMessage = "Thanks for Using Dean and Michel's Vending Machine";
+    public static final String CHOICE_ONE = "1";
+    public static final String CHOICE_TWO = "2";
+    public static final String CHOICE_THREE = "3";
+    public static final String GIVE_CHANGE = "GIVE CHANGE:";
+    public static final String FEED_MONEY = "FEED MONEY:";
+    public static final int DISPLAY_ITEM_MENU_CHARACTER_OFFSET = 7;
+    public static final int MAIN_MENU_CHARACTER_OFFSET = -1;
+    public static final int PURCHASE_MENU_CHARACTER_OFFSET = 1;
+    public static final int FINISHING_MENU_CHARACTER_OFFSET = 2;
+    public static final String PRODUCT_CODE_REGEX_EXPRESSION = "[a-zA-z]{1}[1-9]{1}";
+
 }
--- src/main/java/com/techelevator/VendingMachine.java
+++ src/main/java/com/techelevator/VendingMachine.java
@@ -16,61 +16,77 @@ public class VendingMachine {
     private final Scanner userInputScanner = new Scanner(System.in);
     private final PrintManager printManager = new PrintManager();
     private final InventoryManager inventoryManager = new InventoryManager();
-    private Menu mainMenu = new MainMenu();
     private final Account account = new Account();
-    private final String CHOICE_ONE = "1";
-    private final String CHOICE_TWO = "2";
-    private final String CHOICE_THREE = "3";
     private final Logger log = new Logger();
+
+    // Pull in constants from CustomConstants class
+    private final String CHOICE_ONE = CustomConstants.CHOICE_ONE;
+    private final String CHOICE_TWO = CustomConstants.CHOICE_TWO;
+    private final String CHOICE_THREE = CustomConstants.CHOICE_THREE;
+    private final String GIVE_CHANGE = CustomConstants.GIVE_CHANGE;
+    private final String FEED_MONEY = CustomConstants.FEED_MONEY;
+    private final int DISPLAY_ITEM_MENU_CHARACTER_OFFSET = CustomConstants.DISPLAY_ITEM_MENU_CHARACTER_OFFSET;
+    private final int MAIN_MENU_CHARACTER_OFFSET = CustomConstants.MAIN_MENU_CHARACTER_OFFSET;
+    private final int PURCHASE_MENU_CHARACTER_OFFSET = CustomConstants.PURCHASE_MENU_CHARACTER_OFFSET;
+    private final int FINISHING_MENU_CHARACTER_OFFSET = CustomConstants.FINISHING_MENU_CHARACTER_OFFSET;
+    private final String PRODUCT_CODE_REGEX_EXPRESSION = CustomConstants.PRODUCT_CODE_REGEX_EXPRESSION;
 
     public void run() {
         printManager.printWelcomeBanner();
 
         while (true) { // MAIN MENU LOOP
-            String userInput = "";
+            String userInput;
             do {
-                printManager.printMenu(mainMenu, -1);
+                printManager.printMenu(new MainMenu(), MAIN_MENU_CHARACTER_OFFSET);
                 userInput = userInputScanner.nextLine();
-            } while (!userInput.equals(CHOICE_ONE) && !userInput.equals(CHOICE_TWO) && !userInput.equals(CHOICE_THREE));
+            } while (!userInput.equals(CHOICE_ONE)
+                    && !userInput.equals(CHOICE_TWO)
+                    && !userInput.equals(CHOICE_THREE)
+            );
+
+            // 1 = Display Vending Machine Items
+            // 2 = Purchase
+            // 3 = Exit
 
             switch (userInput) {
                 case CHOICE_ONE:
-                    printManager.printMenu(new DisplayItemMenu(inventoryManager), 7);
+                    printManager.printMenu(new DisplayItemMenu(inventoryManager), DISPLAY_ITEM_MENU_CHARACTER_OFFSET);
                     break;
                 case CHOICE_TWO:
                     purchasingLoop();
                     break;
-                case CHOICE_THREE: // exit
+                case CHOICE_THREE:
                     printManager.printExitBanner();
                     System.exit(0);
             }
         }
-
-
     }
 
     private void purchasingLoop() {
         while (true) {
-            String userPurchasingInput = "";
+            String userPurchasingInput;
             do {
-                printManager.printMenu(new PurchaseMenu(account), 1); // START USER AT PURCHASING MENU
+                printManager.printMenu(new PurchaseMenu(account), PURCHASE_MENU_CHARACTER_OFFSET);
                 userPurchasingInput = userInputScanner.nextLine();
-            } while (!userPurchasingInput.equals(CHOICE_ONE) && !userPurchasingInput.equals(CHOICE_TWO) && !userPurchasingInput.equals(CHOICE_THREE));
+            } while (!userPurchasingInput.equals(CHOICE_ONE)
+                    && !userPurchasingInput.equals(CHOICE_TWO)
+                    && !userPurchasingInput.equals(CHOICE_THREE)
+            );
 
-            // 1 = feed money
-            // 2 = select product
-            // 3 = finish transaction
+            // 1 = Feed Money
+            // 2 = Select Product
+            // 3 = Finish Transaction
 
             switch (userPurchasingInput) {
                 case CHOICE_ONE:
-                    feedMoney();
+                    feedMoneyLoop();
                     break;
                 case CHOICE_TWO:
                     dispensingLoop();
                     break;
                 case CHOICE_THREE:
-                    log.logPurchase("GIVE CHANGE", account.getBalance(), BigDecimal.ZERO);
-                    printManager.printMenu(new FinishingMenu(account), 2);
+                    log.logPurchase(GIVE_CHANGE, account.getBalance(), BigDecimal.ZERO);
+                    printManager.printMenu(new FinishingMenu(account), FINISHING_MENU_CHARACTER_OFFSET);
                     account.setBalanceToZero();
                     return;
             }
@@ -78,12 +94,12 @@ public class VendingMachine {
     }
 
     private void dispensingLoop() {
-        String userDispensingInput = "";
-        printManager.printMenu(new DisplayItemMenu(inventoryManager), 7);
+        String userDispensingInput;
+        printManager.printMenu(new DisplayItemMenu(inventoryManager), DISPLAY_ITEM_MENU_CHARACTER_OFFSET);
         do {
             printManager.printItemCodePrompt();
             userDispensingInput = userInputScanner.nextLine().toLowerCase();
-        } while (!userDispensingInput.matches("[a-zA-z]{1}[1-9]{1}"));
+        } while (!userDispensingInput.matches(PRODUCT_CODE_REGEX_EXPRESSION));
 
         for (Slot slot : inventoryManager.getInventory()) {
             if (userDispensingInput.equals(slot.getLocation().toLowerCase())) {
@@ -107,7 +123,7 @@ public class VendingMachine {
     }
 
 
-    private void feedMoney() {
+    private void feedMoneyLoop() {
         int moneyToAdd;
         while (true) {
             printManager.printFeedMoneyPrompt();
@@ -115,7 +131,7 @@ public class VendingMachine {
                 moneyToAdd = Integer.parseInt(userInputScanner.nextLine());
                 if (moneyToAdd > 0) {
                     account.feedMoney(moneyToAdd);
-                    log.logPurchase("FEED MONEY", BigDecimal.valueOf(moneyToAdd), account.getBalance());
+                    log.logPurchase(FEED_MONEY, BigDecimal.valueOf(moneyToAdd), account.getBalance());
                     break;
                 }
             } else {
